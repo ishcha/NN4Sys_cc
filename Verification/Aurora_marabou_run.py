4d1eaac5d3c65d286353178acd2a050393f70b9a
Shuyi Lin <710417097@qq.com>
2023-10-01 16:37:29 -0400

    -

--- Verification/Aurora_marabou_run.py
+++ Verification/Aurora_marabou_run.py
@@ -28,10 +28,9 @@ def main(marabou_path):
         os.makedirs(running_result_path)
     for spec_type in [4]:
         for num in range(SIZE):
-            os.system(f'cd {marabou_path}')
             os.system(
-                f'python maraboupy/Marabou.py {cur_dir}/{onnx_dir_path}/aurora_mid_{MODEL_TYPES[spec_type]}.onnx {cur_dir}/{txt_dir_path}/aurora_{SPEC_TYPES[spec_type]}_{num}.txt | tee {cur_dir}/{running_result_path}/mid_{MODEL_TYPES[spec_type]}_{SPEC_TYPES[spec_type]}_{num}.txt')
-            os.system(f'cd {cur_dir}')
+                f'python {marabou_path} {onnx_dir_path}/aurora_mid_{MODEL_TYPES[spec_type]}.onnx {txt_dir_path}/aurora_{SPEC_TYPES[spec_type]}_{num}.txt | tee {running_result_path}/mid_{MODEL_TYPES[spec_type]}_{SPEC_TYPES[spec_type]}_{num}.txt')
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
