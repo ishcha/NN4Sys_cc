2dcf3f11b60416152690a8039f8f0a88ae20519d
Shuyi Lin <710417097@qq.com>
2023-10-02 01:32:55 -0400

    -

--- Models/Pensieve/model_no_softmax_no_argmax.py
+++ Models/Pensieve/model_no_softmax_no_argmax.py
@@ -248,6 +248,76 @@ class ActorNetwork_small(nn.Module):
 
     def forward(self, x):
         # x = torch.reshape(x, (1, self.s_dim[0], self.s_dim[1]))
+
+
+        x = x.view([-1, self.s_dim[0], self.s_dim[1]])
+        split_0, split_1, split_2, split_3, split_4_5,a = torch.split(x, [1, 1, 1, 1, 1,1], dim=1)
+
+        a, b, c, d, e, f, g, split_0 = torch.split(split_0, [1, 1, 1, 1, 1, 1, 1, 1], dim=2)
+        split_0 = split_0.view(split_0.shape[0], -1)
+
+        split_0 = self.linear0(split_0)
+        split_0 = self.relu(split_0)
+
+        a, b, c, d, e, f, g, split_1 = torch.split(split_1, [1, 1, 1, 1, 1, 1, 1, 1], dim=2)
+        split_1 = split_1.view(split_1.shape[0], -1)
+
+        split_1 = self.linear1(split_1)
+        split_1 = self.relu(split_1)
+
+        split_2 = self.linear2(split_2)
+        split_2 = self.relu(split_2)
+        split_3 = self.linear3(split_3)
+        split_3 = self.relu(split_3)
+
+        split_4, a, split_5 = torch.split(split_4_5, [A_DIM,1, 1], dim=2)
+        split_4 = self.linear4(split_4)
+        split_4 = self.relu(split_4)
+        split_5 = split_5.view(split_5.shape[0], -1)
+        split_5 = self.linear5(split_5)
+
+
+
+
+
+
+
+
+
+
+
+
+        split_2 = split_2.view(split_2.shape[0], -1)
+        split_3 = split_3.view(split_3.shape[0], -1)
+        split_4 = split_4.view(split_4.shape[0], -1)
+
+        x = torch.cat((split_0, split_1, split_2, split_3, split_4, split_5), 1)
+        x = self.linear6(x)
+        x = self.relu(x)
+        x = self.linear7(x)
+
+        return x
+
+class ActorNetwork_small_bak(nn.Module):
+    def __init__(self, state_dim, action_dim, learning_rate):
+        super().__init__()
+        self.s_dim = state_dim
+        self.a_dim = action_dim
+        self.lr_rate = learning_rate
+
+        self.relu = nn.ReLU()
+        self.linear0 = nn.Linear(1, 128)
+        self.linear1 = nn.Linear(1, 128)
+        self.linear2 = nn.Linear(8, 128)
+        self.linear3 = nn.Linear(8, 128)
+        self.linear4 = nn.Linear(6, 128)
+        self.linear5 = nn.Linear(1, 128)
+
+        self.linear6 = nn.Linear(768, 128)
+        self.linear7 = nn.Linear(128, self.a_dim)
+
+    def forward(self, x):
+        # x = torch.reshape(x, (1, self.s_dim[0], self.s_dim[1]))
         x = x.view([-1, self.s_dim[0], self.s_dim[1]])
 
         split_0 = self.linear0(x[:, 0:1, -1])
