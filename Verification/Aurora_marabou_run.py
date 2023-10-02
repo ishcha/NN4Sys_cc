6f488a0a007727708b8ab31fd27b6ffa6ac20959
Shuyi Lin <710417097@qq.com>
2023-10-01 21:56:55 -0400

    -

--- Verification/Aurora_marabou_run.py
+++ Verification/Aurora_marabou_run.py
@@ -8,7 +8,7 @@ MODEL_TYPES=['simple', 'simple', 'simple', 'parallel', 'concat']
 DIFFICULTY = ['easy']
 running_result_path = './aurora_marabou_running_result'
 SIZES = [10, 10, 10, 10, 10]
-SIZE=50
+SIZE=10
 
 SPEC_TYPES = [101, 102, 2, 3, 4]
 SPEC_ARRAY_LENGTH = [30, 30, 30, 60, 150]
@@ -27,7 +27,7 @@ def main(marabou_path):
     if not os.path.exists(running_result_path):
         os.makedirs(running_result_path)
     for spec_type in range(len(SPEC_TYPES)):
-        for num in range(SIZE):
+        for num in range(SIZES[spec_type]):
             command = f'python {marabou_path} {onnx_dir_path}/aurora_mid_{MODEL_TYPES[spec_type]}.onnx {txt_dir_path}/aurora_{SPEC_TYPES[spec_type]}_{num}.txt | tee {running_result_path}/mid_{MODEL_TYPES[spec_type]}_{SPEC_TYPES[spec_type]}_{num}.txt'
             print(command)
             os.system(command)
