76de4fd38c1113a6272073700fc0a0c2069c560b
Shuyi Lin <710417097@qq.com>
2023-10-03 01:30:35 -0400

    -

--- Verification/abcrown_parser.py
+++ Verification/abcrown_parser.py
@@ -3,64 +3,67 @@ import math
 import numpy as np
 import sys
 
+
 def calculate_avg_time(dic1, dic2, times1, times2):
-    ret={}
+    ret = {}
     for key in times2:
         if key in times1:
-            times1[key]+=times2[key]
+            times1[key] += times2[key]
         else:
             times1[key] = times2[key]
     for key in dic2:
         if key in dic1:
-            dic1[key]+=dic2[key]
+            dic1[key] += dic2[key]
         else:
             dic1[key] = dic2[key]
     for key in dic1:
-        ret[key] = times1[key]/dic1[key]
+        ret[key] = times1[key] / dic1[key]
     return ret
+
+
 def main(model):
     dir = f'./{model}_abcrown_running_result'
     files = os.listdir(dir)
 
     unsat = 0
     sat = 0
-    sat_dic={}
-    unsat_dic={}
-    sat_time={}
-    unsat_time={}
+    sat_dic = {}
+    unsat_dic = {}
+    sat_time = {}
+    unsat_time = {}
     print("sat files:----------------------------------------------------------")
 
     for f in files:
-        file=f'{dir}/'+f
+        file = f'{dir}/' + f
         if file[-3:] != 'txt':
             continue
         index = '_'.join(f[:-4].split('_')[:-1])
-        timeout=-1
+        timeout = -1
 
-        with open(file,'r') as f:
-            result=''
+        with open(file, 'r') as f:
+            result = ''
             for line in f:
                 line = line.strip()
                 if line[:6] == "Result":
                     result = line[8:]
                 if line[:4] == "Time":
                     timeout = float(line[6:15])
-            if timeout==-1:
+            if timeout == -1:
                 continue
             timeout = math.ceil(timeout)
-            if result=='unsat':
-                unsat+=1
+            if result == 'unsat':
+                unsat += 1
                 if index in unsat_dic.keys():
-                    unsat_dic[index] = unsat_dic[index]+1
+                    unsat_dic[index] = unsat_dic[index] + 1
                     unsat_time[index] = unsat_time[index] + timeout
                 else:
                     unsat_dic[index] = 1
                     unsat_time[index] = timeout
-            elif result=='sat':
+            elif result == 'sat':
                 print(file)
-                sat+=1
+                sat += 1
                 if index in sat_dic.keys():
-                    sat_dic[index] = sat_dic[index]+1
+                    sat_dic[index] = sat_dic[index] + 1
                     sat_time[index] = sat_dic[index] + timeout
                 else:
                     sat_dic[index] = 1
@@ -75,19 +78,15 @@ def main(model):
 
     sat_dic = dict(sorted(sat_dic.items()))
     unsat_dic = dict(sorted(unsat_dic.items()))
-
-    avg_time = calculate_avg_time(sat_dic, unsat_dic, sat_time, unsat_time)
-
-
-
     print("sat")
     print(len(sat_dic))
     print(sat_dic)
 
-
     print("unsat")
     print(len(unsat_dic))
     print(unsat_dic)
+
+    avg_time = calculate_avg_time(sat_dic, unsat_dic, sat_time, unsat_time)
     print(avg_time)
 
 
