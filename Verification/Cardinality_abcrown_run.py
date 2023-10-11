230585743607063823cd6f04f0c06d23e067239e
Shuyi Lin <710417097@qq.com>
2023-10-10 22:58:52 -0400

    add bloom filter

--- Verification/Cardinality_abcrown_run.py
+++ Verification/Cardinality_abcrown_run.py
@@ -3,21 +3,15 @@ import sys
 
 os.environ['MKL_THREADING_LAYER'] = 'GNU'
 
-MODELS1 = ["cardinality_128", "cardinality_2048"]
-MODELS2 = ["cardinality_128_dual",  "cardinality_2048_dual"]
+MODEL_NAMES=["lindex","lindex_deep"]
+SIZE=10
 
-SIZES = [10, 10, 10, 10, 10]
-SIZE = 10
-
-
-
-model_name = "cardinality"
 
 # create yaml
 vnn_dir_path = '../Benchmarks/vnnlib'
 onnx_dir_path = '../Benchmarks/onnx'
-yaml_path = f'./{model_name}_yaml'
-running_result_path = f'./{model_name}_abcrown_running_result'
+yaml_path = './lindex_yaml'
+running_result_path = './lindex_abcrown_running_result'
 timeout = 100
 csv_data = []
 total_num = 0
@@ -28,43 +22,38 @@ if not os.path.exists(running_result_path):
 if not os.path.exists(yaml_path):
     os.makedirs(yaml_path)
 
-def create_yaml(yaml, vnn_path, onnx_path, inputshape=6):
+def create_yaml(yaml, vnn_path, onnx_path):
     with open(yaml, mode='w') as f:
-        f.write("general:\n  enable_incomplete_verification: False\n  conv_mode: matrix\n")
-        f.write(f'model:\n  onnx_path: {onnx_path}\n')
-        f.write(f'specification:\n  vnnlib_path: {vnn_path}\n')
-        f.write(
-            "solver:\n  batch_size: 2048\nbab:\n  branching:\n    method: sb\n    sb_coeff_thresh: 0.1\n    input_split:\n      enable: True")
+
+        f.write(f"general:\n  enable_incomplete_verification: False\n  loss_reduction_func: max\n  conv_mode: matrix\nmodel:\n  onnx_path: {onnx_path}\n")
+
+        f.write(f"specification:\n  vnnlib_path: {vnn_path}\nsolver:\n  "
+                f"batch_size: 50  # Number of parallel domains to compute on GPU.\n  bound_prop_method: forward+backward\n  beta-crown:\n"
+                f"    iteration: 10  # Iterations for computing intermediate layer bounds.\n")
+        f.write(f"bab:\n  initial_max_domains: 100000\n  branching:\n    method: naive  # Split on input space.\n    input_split:\n"
+                f"      enable: True\n      adv_check: .inf\n")
+        #f.write(f"attack:\n  pgd_order: skip")
 
 
+
+
+
+dif = [1,200,400,600,800,1000,3000,5000,7000,9000,10000]
 def main(abcrown_path):
-    for MODEL in MODELS1:
-        for size in range(SIZE):
-            vnn_path = f'{vnn_dir_path}/{MODEL}_1_{size}.vnnlib'
-            onnx_path = f'{onnx_dir_path}/{MODEL}.onnx'
-            yaml = f'{yaml_path}/{MODEL}_1_{size}.yaml'
+    model_name = "lindex"
+    for i in range(SIZE):
+        for model in MODEL_NAMES:
+            vnn_path = f'{vnn_dir_path}/lindex_{dif[i]}.vnnlib'
+            onnx_path =  f'{onnx_dir_path}/{model}.onnx'
+            yaml = yaml_path + f'/{model_name}_{i}.yaml'
             create_yaml(yaml, vnn_path, onnx_path)
-            os.system(f"python {abcrown_path} --config {yaml} | tee {running_result_path}/{MODEL}_1_{size}.txt")
-
-            vnn_path = f'{vnn_dir_path}/{MODEL}_2_{size}.vnnlib'
-            yaml = f'{yaml_path}/{MODEL}_2_{size}.yaml'
-            create_yaml(yaml, vnn_path, onnx_path)
-            os.system(f"python {abcrown_path} --config {yaml} | tee {running_result_path}/{MODEL}_2_{size}.txt")
-
-    for MODEL in MODELS2:
-        for size in range(SIZE):
-            vnn_path = f'{vnn_dir_path}/{MODEL}_{size}.vnnlib'
-            onnx_path = f'{onnx_dir_path}/{MODEL}.onnx'
-            yaml = f'{yaml_path}/{MODEL}_{size}.yaml'
-            create_yaml(yaml, vnn_path, onnx_path)
-            os.system(f"python {abcrown_path} --config {yaml} | tee {running_result_path}/{MODEL}_{size}.txt")
-
+            os.system(f"python {abcrown_path} --config {yaml} | tee {running_result_path}/{model}_{i}.txt")
 
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: Cadinality_abcorwn_run.py abcrown_path")
+        print("Usage: Lindex_abcorwn_run.py abcrown_path")
         exit(1)
     abcrown_path = sys.argv[1]
     main(abcrown_path)
