8ccb776cd5200443870e208bc47da437ddc5be64
Shuyi Lin <710417097@qq.com>
2023-10-11 01:30:26 -0400

    add bloom filter

--- Verification/Lindex_marabou_run.py
+++ Verification/Lindex_marabou_run.py
@@ -17,10 +17,11 @@ onnx_dir_path = '../Benchmarks/onnx'
 def main(marabou_path):
     if not os.path.exists(running_result_path):
         os.makedirs(running_result_path)
+    model_name = "lindex"
 
     for model in MODEL_NAMES:
         for num in range(SIZE):
-            command = f'python {marabou_path} {onnx_dir_path}/{model}.onnx {txt_dir_path}/{model}_0_{num}.txt | tee {running_result_path}/{model}_0_{num}.txt'
+            command = f'python {marabou_path} {onnx_dir_path}/{model}.onnx {txt_dir_path}/{model_name}_0_{num}.txt | tee {running_result_path}/{model}_0_{num}.txt'
             print(command)
             os.system(command)
 
