b587eba528dc0e4f27c1b675ec6164759bba1641
Shuyi Lin <710417097@qq.com>
2023-10-20 00:38:19 -0400

    -

--- Benchmarks/generate_properties.py
+++ Benchmarks/generate_properties.py
@@ -3,11 +3,15 @@ import sys
 import csv
 from haoyu_gen import main as card_main
 from cheng_gen import main as index_main
+from decima_gen import main as decima_main
+from aurora_gen import main as aurora_main
+from pensieve_gen import main as pensieve_main
+from bloom_filter_gen import main as bloom_filter
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        #print("Usage: generate_properties.py <random seed>")
-        seed=2024
+        print("Usage: generate_properties.py <random seed>, default is 2024")
+        seed = 2024
     else:
         seed = sys.argv[1]
 
@@ -15,12 +19,15 @@ if __name__ == "__main__":
         os.makedirs('vnnlib')
 
     csv_data = []
-
     print("Generating Index specifications, this may take several minutes...")
     csv_data.extend(index_main(seed))
     print("Generating cardinality specifications, this may take around ten minutes...")
     csv_data.extend(card_main(seed))
     print(f"Successfully generate {len(csv_data)} files!")
+    decima_main(seed)
+    aurora_main(seed)
+    pensieve_main(seed)
+    bloom_filter(seed)
     print(f"Total timeout is {sum([int(i[-1]) for i in csv_data])}")
     with open('instances.csv', 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
---
+++ Models/gen_upper.py
@@ -0,0 +1,66 @@
+import argparse
+
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--model", default="all",
+                        choices=["pensieve", "decima", "lindex", "cardinality", "bloom_filter", "aurora", "all"],
+                        help="which model to verify pensieve, decima, lindex, cardinality, bloom_filter, aurora")
+
+    args = parser.parse_args()
+    if args.model == "all":
+        os.system("cd Pensieve")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+
+        os.system("cd Aurora")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+
+        os.system("cd Decima")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+
+        os.system("cd Learned_index")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+
+        os.system("cd Cardinality")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+
+        os.system("cd Bloom_filter")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+
+    if args.model == "pensieve":
+        os.system("cd Pensieve")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+    if args.model == "decima":
+        os.system("cd Decima")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+    if args.model == "lindex":
+        os.system("cd Learned_index")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+    if args.model == "cardinality":
+        os.system("cd Cardinality")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+    if args.model == "bloom_filter":
+        os.system("cd Bloom_filter")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+    if args.model == "aurora":
+        os.system("cd Aurora")
+        os.system("python shuyi_gen_upper.py")
+        os.system("cd ..")
+
+
+if __name__ == "__main__":
+    main()
