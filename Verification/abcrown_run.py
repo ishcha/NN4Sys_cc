65d4ccc65d94b15f3ed0f0d890258b40bbbee371
Shuyi Lin <710417097@qq.com>
2023-10-19 21:45:57 -0400

    -

--- Verification/abcrown_run.py
+++ Verification/abcrown_run.py
@@ -13,27 +13,29 @@ def main():
     parser.add_argument("--model", default="all",
                         choices=["pensieve", "decima", "lindex", "cardinality", "bloom_filter", "aurora", "all"],
                         help="which model to verify pensieve, decima, lindex, cardinality, bloom_filter, aurora")
+    parser.add_argument("--path",
+                        help="verifier path")
 
     args = parser.parse_args()
     if args.model == "all":
-        Pensieve_abcrown_run.main()
-        Decima_abcrown_run.main()
-        Lindex_abcrown_run.main()
-        Cardinality_abcrown_run.main()
-        Bloom_filter_abcrown_run.main()
-        Aurora_abcrown_run.main()
+        Pensieve_abcrown_run.main(args.path)
+        Decima_abcrown_run.main(args.path)
+        Lindex_abcrown_run.main(args.path)
+        Cardinality_abcrown_run.main(args.path)
+        Bloom_filter_abcrown_run.main(args.path)
+        Aurora_abcrown_run.main(args.path)
     if args.model == "pensieve":
-        Pensieve_abcrown_run.main()
+        Pensieve_abcrown_run.main(args.path)
     if args.model == "decima":
-        Decima_abcrown_run.main()
+        Decima_abcrown_run.main(args.path)
     if args.model == "lindex":
-        Lindex_abcrown_run.main()
+        Lindex_abcrown_run.main(args.path)
     if args.model == "cardinality":
-        Cardinality_abcrown_run.main()
+        Cardinality_abcrown_run.main(args.path)
     if args.model == "bloom_filter":
-        Bloom_filter_abcrown_run.main()
+        Bloom_filter_abcrown_run.main(args.path)
     if args.model == "aurora":
-        Aurora_abcrown_run.main()
+        Aurora_abcrown_run.main(args.path)
 
 
 if __name__ == "__main__":
